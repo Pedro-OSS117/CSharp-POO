3998f8a1bc9a2a5f902825d83b54cd58feec8774
Pedro-OSS117 <pierreproudhon@gmail.com>
2021-01-16 17:40:48 +0100

    Cours Les Pointeurs

---
+++ 00 - Les Pointeurs/Cours.md
@@ -0,0 +1,7 @@
+Les Pointeurs
+
+I - Les Types Valeurs - Notion de copie
+
+II - Les Pointeurs - Adresse Memoire
+
+III - Le problèmes des pointeurs en CSharp - Garbage Collector
---
+++ 00 - Les Pointeurs/ProjetCours/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProjetCours
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Les Pointeurs !");
+        }
+    }
+}
